484a3790c5accec89552a4ce3a8bc5b3f8daeaae
Tapabrata Jana <tapabratajana01@gmail.com>
2024-09-04 12:46:50 +0530

    my first php project

---
+++ info.php
@@ -0,0 +1,11 @@
+<?php //php start tag
+
+echo 'Hello<br>';
+
+echo("my name is Tapabrata!<br>");
+
+Print ('All code is executing in server side.<br>');
+
+?>  
+
+All code is executing in client side.<br>
